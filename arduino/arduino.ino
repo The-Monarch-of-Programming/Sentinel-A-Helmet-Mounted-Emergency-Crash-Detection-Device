1e861e5c9348b595887bfe507138da4749bb70fb
Hanz Jibriel Agbayani <qhjcagbayani@tip.edu.ph>
2026-04-16 18:36:01 +0800

    fixes

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,9 +4,8 @@
 #include <SoftwareSerial.h>
 
 MPU6050 mpu;
-
 TinyGPSPlus gps;
-SoftwareSerial gpsSerial(4, 3);
+SoftwareSerial gpsSerial(4, 5); // RX, TX
 
 int impactPin = 7;
 int ledPin = 13;
@@ -15,26 +14,34 @@ int confirmCount = 0;
 int confirmThreshold = 3;
 
 float data[5];
-
 bool impactSent = false;
 
 // Thresholds
 float lowThreshold = 1;
-float highThreshold = 2;
+float mediumThreshold = 2;
+float highThreshold = 3;
 
 float filteredAcc = 0;
-
 String severity = "NONE";
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+
   gpsSerial.begin(9600);
 
   pinMode(impactPin, INPUT);
   pinMode(ledPin, OUTPUT);
 
   mpu.initialize();
+  delay(1000);
+
+  if (!mpu.testConnection()) {
+    Serial.println("MPU6050 connection FAILED");
+    while (1);
+  } else {
+    Serial.println("MPU6050 connected");
+  }
 }
 
 void loop() {
@@ -60,6 +67,7 @@ void loop() {
   int16_t ax, ay, az;
   mpu.getAcceleration(&ax, &ay, &az);
 
+  // Convert to G-force
   data[0] = ax / 16384.0;
   data[1] = ay / 16384.0;
   data[2] = az / 16384.0;
@@ -72,53 +80,43 @@ void loop() {
 
   filteredAcc = 0.7 * filteredAcc + 0.3 * totalAcc;
 
-  severity = "NONE";
-
   if (impactDetected) {
     if (filteredAcc > highThreshold) {
-      severity = "HIGH";
+      severity = "High";
     } 
-    else if (filteredAcc > lowThreshold) {
-      severity = "LOW";
+    else if (filteredAcc > mediumThreshold) {
+      severity = "Medium";
     }
     else {
-      severity = "NONE";
+      severity = "Low";
     }
   }
 
   digitalWrite(ledPin, impactDetected ? HIGH : LOW);
 
-  if (impactDetected && !impactSent) {
+ if (impactDetected && !impactSent) {
 
-    impactSent = true;
+  impactSent = true;
 
-    Serial.println("Impact Detected");
-
-    // GPS
-    if (gps.location.isValid() && gps.location.isUpdated()) {
-      data[3] = gps.location.lat();
-      data[4] = gps.location.lng();
-    } else {
-      data[3] = 0;
-      data[4] = 0;
-    }
-
-    float speed = gps.speed.isValid() ? gps.speed.kmph() : 0;
-
-    Serial.print("Severity: ");
-    Serial.println(severity);
-
-    Serial.print("Accelerometer: ");
-    Serial.print(data[0]); Serial.print(",");
-    Serial.print(data[1]); Serial.print(",");
-    Serial.print(data[2]); Serial.print(",");
-    Serial.println(speed);
-
-    Serial.print("Latitude: ");
-    Serial.print(data[3], 6);
-    Serial.print(" Longitude: ");
-    Serial.println(data[4], 6);
+  // GPS
+  if (gps.location.isValid()) {
+    data[3] = gps.location.lat();
+    data[4] = gps.location.lng();
+  } else {
+    data[3] = 0;
+    data[4] = 0;
   }
 
-  delay(50);
+  float speed = gps.speed.isValid() ? gps.speed.kmph() : 0;
+
+  Serial.print(data[0]); Serial.print(",");
+  Serial.print(data[1]); Serial.print(",");
+  Serial.print(data[2]); Serial.print(",");
+  Serial.print(speed); Serial.print(",");
+  Serial.print(data[3], 6); Serial.print(",");
+  Serial.print(data[4], 6); Serial.print(",");
+  Serial.println(severity);
+}
+
+  delay(200);
 }
